0d24e57e6de3a7ed87b5271cb302fab8dafb64b3
giongiorgiopietro-design <giongiorgiopietro@gmail.com>
2026-06-18 18:16:30 +0200

    config.py

--- modules/config.py
+++ modules/config.py
@@ -1,1 +1,16 @@
+from dataclasses import dataclass
 
+@dataclass
+class Config:
+
+    DEPTH_MODEL = "depth-anything/Depth-Anything-V2-Small-hf"
+
+    OUTPUT_FOLDER = "outputs"
+
+    NORMAL_STRENGTH = 2.0
+
+    HYPERDETAIL = 1.8
+
+    SAVE_16BIT = True
+
+cfg = Config()
