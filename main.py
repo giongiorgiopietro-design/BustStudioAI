50fedf2c740cfed0002eeb95e006e753e39ad313
giongiorgiopietro-design <giongiorgiopietro@gmail.com>
2026-06-18 19:12:54 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from modules.project import Project
+from modules.depth import DepthEngine
+from modules.normal import NormalEngine
+from modules.hyperdepth import HyperDepthEngine
+
+
+def main():
+
+    project = Project(image_path="foto.jpg")
+
+    project.load_image()
+
+    depth = DepthEngine()
+    depth.run(project)
+
+    normal = NormalEngine()
+    normal.run(project)
+
+    hyper = HyperDepthEngine()
+    hyper.run(project)
+
+    print("\n✅ Elaborazione completata!")
+    print("File salvati nella cartella outputs/")
+
+
+if __name__ == "__main__":
+    main()
